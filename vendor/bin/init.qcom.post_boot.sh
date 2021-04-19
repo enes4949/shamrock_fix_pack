ab51a9084203476cd145c5c2c8b31ff0367e5eb6
Your Name <you@example.com>
2021-04-19 20:25:19 +0300

    depolama duzeltmeleri

--- vendor/bin/init.qcom.post_boot.sh
+++ vendor/bin/init.qcom.post_boot.sh
@@ -256,25 +256,13 @@ function configure_read_ahead_kb_values() {
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
     MemTotal=${MemTotalStr:16:8}
 
-    # Set 128 for <= 3GB &
-    # set 512 for >= 4GB targets.
-    if [ $MemTotal -le 3145728 ]; then
-        echo 128 > /sys/block/mmcblk0/bdi/read_ahead_kb
-        echo 128 > /sys/block/mmcblk0/queue/read_ahead_kb
-        echo 128 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
-        echo 128 > /sys/block/mmcblk0rpmb/queue/read_ahead_kb
-        echo 128 > /sys/block/dm-0/queue/read_ahead_kb
-        echo 128 > /sys/block/dm-1/queue/read_ahead_kb
-        echo 128 > /sys/block/dm-2/queue/read_ahead_kb
-    else
-        echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
-        echo 512 > /sys/block/mmcblk0/queue/read_ahead_kb
-        echo 512 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
-        echo 512 > /sys/block/mmcblk0rpmb/queue/read_ahead_kb
-        echo 512 > /sys/block/dm-0/queue/read_ahead_kb
-        echo 512 > /sys/block/dm-1/queue/read_ahead_kb
-        echo 512 > /sys/block/dm-2/queue/read_ahead_kb
-    fi
+    echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
+    echo 512 > /sys/block/mmcblk0/queue/read_ahead_kb
+    echo 512 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
+    echo 512 > /sys/block/mmcblk0rpmb/queue/read_ahead_kb
+    echo 512 > /sys/block/dm-0/queue/read_ahead_kb
+    echo 512 > /sys/block/dm-1/queue/read_ahead_kb
+    echo 512 > /sys/block/dm-2/queue/read_ahead_kb
 }
 
 function disable_core_ctl() {
@@ -4341,3 +4329,5 @@ setprop persist.vendor.mmi.misc_dev_path $real_path
 echo '3596' > /sys/class/timed_output/vibrator/vmax_mv_strong # titresimi %100 gucte calistir
 echo 'fiops' > /sys/block/mmcblk0/queue/scheduler # data scheduler
 echo 'fiops' > /sys/block/mmcblk1/queue/scheduler # sd kart scheduler
+echo '0' > /sys/block/mmcblk0/queue/iostats 
+echo '0' > /sys/block/mmcblk0/queue/add_random
